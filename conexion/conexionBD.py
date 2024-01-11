f8efa8e1ff8d7072ed175a29dfaa4de5b1b1f052
aroncgg <151040630+aroncgg@users.noreply.github.com>
2024-01-11 07:29:59 -0500

    My App (templates)

---
+++ conexion/conexionBD.py
@@ -0,0 +1,30 @@
+
+
+# Importando Libreria mysql.connector para conectar Python con MySQL
+import mysql.connector
+
+
+def connectionBD():
+    print("ENTRO A LA CONEXION")
+    try:
+        # connection = mysql.connector.connect(
+        connection = mysql.connector.connect(
+            host="monorail.proxy.rlwy.net",
+                #host="viaduct.proxy.rlwy.net",
+            port=26025,
+            user="root",
+            passwd="e3GEd6BcFCFfHB1cha4g3-HA6a-56H1f",
+                #passwd="-D2eD6aDb5Bg6dEbhAAeBB6gd3EheaBg",
+            database="railway",
+                #database="crud_python",
+            charset='utf8mb4',
+            collation='utf8mb4_unicode_ci',
+            raise_on_warnings=True
+
+        )
+        if connection.is_connected():
+            print("Conexión exitosa a la BD")
+            return connection
+
+    except mysql.connector.Error as error:
+        print(f"No se pudo conectar: {error}")
